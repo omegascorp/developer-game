c79963e4985d08117ed4cba7f1dae8711730e7d7
omegascorp <omegascorp@gmail.com>
2018-02-12 17:45:44 +0400

    Init app with create-react-native-app instead of expo.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import AppView from './src/views/AppView';
 
 export default class App extends React.Component {
@@ -8,3 +9,12 @@ export default class App extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "" ]; then
-    echo "Please provide the host"
-    exit 0
-fi
-
-REACT_NATIVE_PACKAGER_HOSTNAME="$1" exp start
